7d6f51586f7c1e15b3c77958163b3fe11517734a
botlikes <bradwoo8621@yandex.com>
2021-02-21 13:25:42 +0800

    initialize

--- src/console/connected-space/subject-view/report/editor/styles/index.tsx
+++ src/console/connected-space/subject-view/report/editor/styles/index.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
+import { useForceUpdate } from '../../../../../../basic-widgets/utils';
 import { Lang } from '../../../../../../langs';
 import { Report } from '../../../../../../services/tuples/report-types';
+import DefaultTheme from '../../../../../../theme/default-theme';
 import { useReportEditEventBus } from '../report-edit-event-bus';
 import { ReportEditEventTypes } from '../report-edit-event-bus-types';
 import { Section } from '../settings-widgets/section';
@@ -12,22 +14,25 @@ export const BasicStylesSection = (props: { report: Report }) => {
 	const { chart: { settings } } = report;
 
 	const { fire } = useReportEditEventBus();
+	const forceUpdate = useForceUpdate();
 	const onColorChange = (prop: 'color' | 'backgroundColor' | 'borderColor') => (color: string) => {
 		if (!report.chart.settings) {
 			report.chart.settings = {};
 		}
 		report.chart.settings[prop] = color;
 		fire(ReportEditEventTypes.BASIC_STYLE_CHANGED, report);
+		forceUpdate();
 	};
 
 	return <Section title={Lang.CHART.SECTION_TITLE_BASIC_STYLE}>
 		<PropName>{Lang.CHART.COLOR}</PropName>
 		<PropValue>
-			<ChartColorPicker color={settings?.color || 'red'} onChange={onColorChange('color')}/>
+			<ChartColorPicker color={settings?.color || DefaultTheme.fontColor} onChange={onColorChange('color')}/>
 		</PropValue>
 		<PropName>{Lang.CHART.BACKGROUND_COLOR}</PropName>
 		<PropValue>
-			<ChartColorPicker color={settings?.backgroundColor || ''} onChange={onColorChange('backgroundColor')}/>
+			<ChartColorPicker color={settings?.backgroundColor || DefaultTheme.bgColor}
+			                  onChange={onColorChange('backgroundColor')}/>
 		</PropValue>
 		<PropName>{Lang.CHART.BORDER}</PropName>
 		<PropValue/>
@@ -35,7 +40,8 @@ export const BasicStylesSection = (props: { report: Report }) => {
 		<PropValue/>
 		<PropName>{Lang.CHART.BORDER_COLOR}</PropName>
 		<PropValue>
-			<ChartColorPicker color={settings?.borderColor || ''} onChange={onColorChange('borderColor')}/>
+			<ChartColorPicker color={settings?.borderColor || DefaultTheme.borderColor}
+			                  onChange={onColorChange('borderColor')}/>
 		</PropValue>
 		<PropName>{Lang.CHART.BORDER_RADIUS}</PropName>
 		<PropValue/>
