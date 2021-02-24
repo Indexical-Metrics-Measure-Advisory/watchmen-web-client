33d20f11b1be29a0e534643fdc77a7627bbe3ecb
botlikes <bradwoo8621@yandex.com>
2021-02-24 21:50:25 +0800

    initialize

---
+++ src/console/dashboard/header/widgets.ts
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+import { PageHeaderHolderContainer } from '../../../basic-widgets/page-header';
+
+export const DashboardPageHeaderHolder = styled(PageHeaderHolderContainer)`
+	@media print {
+		display : none;
+	}
+`;
