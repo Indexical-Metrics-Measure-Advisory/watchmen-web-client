3504780f0a150ce9961cb138bfdc47370b7346c7
botlikes <bradwoo8621@yandex.com>
2021-02-26 22:39:03 +0800

    initialize

--- src/services/console/connected-space.ts
+++ src/services/console/connected-space.ts
@@ -3,5 +3,6 @@ import { Subject } from '../tuples/subject-types';
 
 export const buildSubjectShareUrl = async (connectedSpace: ConnectedSpace, subject: Subject): Promise<string> => {
 	// REMOTE use real api to retrieve subject share url
-	return `${window.location.href}/share/subject/${subject.subjectId}`;
+	const { protocol, host } = window.location;
+	return `${protocol}//${host}/share/subject/${subject.subjectId}`;
 };
--- src/services/console/dashboard.ts
+++ src/services/console/dashboard.ts
@@ -2,5 +2,6 @@ import { Dashboard } from '../tuples/dashboard-types';
 
 export const buildDashboardShareUrl = async (dashboard: Dashboard): Promise<string> => {
 	// REMOTE use real api to retrieve dashboard share url
-	return `${window.location.href}/share/dashboard/${dashboard.dashboardId}`;
+	const { protocol, host } = window.location;
+	return `${protocol}//${host}/share/dashboard/${dashboard.dashboardId}`;
 };
