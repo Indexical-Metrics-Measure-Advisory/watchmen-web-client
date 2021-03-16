50f0d3a69b83973a696206ce48b5379948e6716e
botlikes <bradwoo8621@yandex.com>
2021-03-16 18:27:45 +0800

    initialize

--- src/admin/enums/items/items-export-button.tsx
+++ src/admin/enums/items/items-export-button.tsx
@@ -1,4 +1,5 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import dayjs from 'dayjs';
 import React from 'react';
 import { DwarfButton } from '../../../basic-widgets/button';
 import { ICON_DOWNLOAD } from '../../../basic-widgets/constants';
@@ -9,7 +10,15 @@ export const ItemsExportButton = (props: { enumeration: Enum }) => {
 	const { enumeration } = props;
 
 	const onExportClicked = () => {
-		// TODO
+		const content = 'code,label,parentCode\n' + enumeration.items.map(item => {
+			return `${item.code || ''},${item.label || ''},${item.parentCode || ''}`;
+		}).join('\n');
+		const link = document.createElement('a');
+		link.href = 'data:text/csv;charset=utf-8,' + encodeURI(content);
+		link.target = '_blank';
+		//provide the name for the CSV file to be downloaded
+		link.download = `enum-${enumeration.name}-${dayjs().format('YYYYMMDDHHmmss')}.csv`;
+		link.click();
 	};
 
 	return <DwarfButton ink={ButtonInk.PRIMARY} onClick={onExportClicked}>
