ef856b8c7d0a4647e0019e4126860cf76f44d1d5
botlikes <bradwoo8621@yandex.com>
2021-04-23 21:25:50 +0800

    initialize

--- src/admin/pipelines/pipeline/body/dsl/unit-part.tsx
+++ src/admin/pipelines/pipeline/body/dsl/unit-part.tsx
@@ -5,13 +5,15 @@ import { Pipeline } from '../../../../../services/tuples/pipeline-types';
 import { Topic } from '../../../../../services/tuples/topic-types';
 import { ActionsPart } from './actions-part';
 import { ConditionalLine } from './conditonal';
-import { PropNameInListFirst } from './dsl-widgets';
+import { PropName, PropNameInListFirst, PropValue } from './dsl-widgets';
 
 export const UnitPart = (props: { pipeline: Pipeline, stage: PipelineStage, unit: PipelineStageUnit, topicsMap: Map<string, Topic> }) => {
 	const { pipeline, stage, unit, topicsMap } = props;
 
 	return <>
 		<PropNameInListFirst indent={2}>- unit</PropNameInListFirst>
+		<PropName indent={4}>name</PropName>
+		<PropValue>{unit.name}</PropValue>
 		<ConditionalLine conditional={unit} topicsMap={topicsMap} indent={4}/>
 		<ActionsPart pipeline={pipeline} stage={stage} unit={unit} topicsMap={topicsMap}/>
 	</>;
