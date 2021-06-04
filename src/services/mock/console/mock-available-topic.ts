66fa3ca4b8c108f88ba70ef02baaae4bedabb18a
botlikes <bradwoo8621@yandex.com>
2021-06-04 15:36:25 +0800

    dqc

--- src/services/mock/console/mock-available-topic.ts
+++ src/services/mock/console/mock-available-topic.ts
@@ -2,7 +2,7 @@ import {Topic} from '../../tuples/topic-types';
 import {DemoTopics} from '../tuples/mock-data-topics';
 
 export const fetchMockAvailableTopics = async (): Promise<Array<Topic>> => {
-	return new Promise(resolve => {
+	return new Promise<Array<Topic>>(resolve => {
 		setTimeout(() => resolve(DemoTopics), 500);
 	});
 };
