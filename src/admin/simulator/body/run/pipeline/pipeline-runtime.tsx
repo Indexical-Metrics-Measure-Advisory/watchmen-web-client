176336a36918ef715fba06af4f3e35eadf9b287a
botlikes <bradwoo8621@yandex.com>
2021-06-07 16:09:18 +0800

    correct export file name

--- src/admin/simulator/body/run/pipeline/pipeline-runtime.tsx
+++ src/admin/simulator/body/run/pipeline/pipeline-runtime.tsx
@@ -220,10 +220,7 @@ export const PipelineRuntime = (props: {
 		const link = document.createElement('a');
 		link.href = 'data:application/json;charset=utf-8,' + encodeURI(JSON.stringify(content));
 		link.target = '_blank';
-		link.download = `
-						data - of - pipeline -${dayjs().format('YYYYMMDDHHmmss')}
-					.
-						json`;
+		link.download = `data-of-pipeline -${dayjs().format('YYYYMMDDHHmmss')}.json`;
 		link.click();
 	};
 	const onDataClicked = async () => {
