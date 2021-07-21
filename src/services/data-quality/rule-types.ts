cd0e8234834ebfce4ee14680e9c4e2060dccd563
luke0623 <luke0623@outlook.com>
2021-07-21 16:23:01 +0800

    Update rule-types.ts

--- src/services/data-quality/rule-types.ts
+++ src/services/data-quality/rule-types.ts
@@ -1,7 +1,7 @@
 export enum MonitorRuleGrade {
-	GLOBAL = 'global',
-	TOPIC = 'topic',
-	FACTOR = 'factor'
+	GLOBAL = "global",
+	TOPIC = "topic",
+	FACTOR = "factor",
 }
 
 export interface MonitorRulesCriteria {
@@ -11,65 +11,65 @@ export interface MonitorRulesCriteria {
 
 export enum MonitorRuleCode {
 	// structure
-	RAW_MISMATCH_STRUCTURE = 'raw-mismatch-structure',
+	RAW_MISMATCH_STRUCTURE = "raw-mismatch-structure",
 
 	// type
-	FACTOR_MISMATCH_ENUM = 'factor-mismatch-enum',
-	FACTOR_MISMATCH_TYPE = 'factor-mismatch-type',
-	FACTOR_MISMATCH_DATE_TYPE = 'factor-mismatch-date-type',
+	FACTOR_MISMATCH_ENUM = "factor-mismatch-enum",
+	FACTOR_MISMATCH_TYPE = "factor-mismatch-type",
+	FACTOR_MISMATCH_DATE_TYPE = "factor-mismatch-date-type",
 
 	// topic row count
-	ROWS_NOT_EXISTS = 'rows-not-exists',
-	ROWS_NO_CHANGE = 'rows-no-change',
-	ROWS_COUNT_MISMATCH_AND_ANOTHER = 'rows-count-mismatch-and-another',
+	ROWS_NOT_EXISTS = "rows-not-exists",
+	ROWS_NO_CHANGE = "rows-no-change",
+	ROWS_COUNT_MISMATCH_AND_ANOTHER = "rows-count-mismatch-and-another",
 
 	// for all factor types
-	FACTOR_IS_EMPTY = 'factor-is-empty',
-	FACTOR_USE_CAST = 'factor-use-cast',
-	FACTOR_COMMON_VALUE_OVER_COVERAGE = 'factor-common-value-over-coverage',
-	FACTOR_EMPTY_OVER_COVERAGE = 'factor-empty-over-coverage',
+	FACTOR_IS_EMPTY = "factor-is-empty",
+	FACTOR_USE_CAST = "factor-use-cast",
+	FACTOR_COMMON_VALUE_OVER_COVERAGE = "factor-common-value-over-coverage",
+	FACTOR_EMPTY_OVER_COVERAGE = "factor-empty-over-coverage",
 
 	// for number type
-	FACTOR_BREAKS_MONOTONE_INCREASING = 'factor-breaks-monotone-increasing',
-	FACTOR_BREAKS_MONOTONE_DECREASING = 'factor-breaks-monotone-decreasing',
-	FACTOR_NOT_IN_RANGE = 'factor-not-in-range',
-	FACTOR_MAX_NOT_IN_RANGE = 'factor-max-not-in-range',
-	FACTOR_MIN_NOT_IN_RANGE = 'factor-min-not-in-range',
-	FACTOR_AVG_NOT_IN_RANGE = 'factor-avg-not-in-range',
-	FACTOR_MEDIAN_NOT_IN_RANGE = 'factor-median-not-in-range',
-	FACTOR_QUANTILE_NOT_IN_RANGE = 'factor-quantile-not-in-range',
-	FACTOR_STDEV_NOT_IN_RANGE = 'factor-stdev-not-in-range',
-	FACTOR_COMMON_VALUE_NOT_IN_RANGE = 'factor-common-value-not-in-range',
+	FACTOR_BREAKS_MONOTONE_INCREASING = "factor-breaks-monotone-increasing",
+	FACTOR_BREAKS_MONOTONE_DECREASING = "factor-breaks-monotone-decreasing",
+	FACTOR_NOT_IN_RANGE = "factor-not-in-range",
+	FACTOR_MAX_NOT_IN_RANGE = "factor-max-not-in-range",
+	FACTOR_MIN_NOT_IN_RANGE = "factor-min-not-in-range",
+	FACTOR_AVG_NOT_IN_RANGE = "factor-avg-not-in-range",
+	FACTOR_MEDIAN_NOT_IN_RANGE = "factor-median-not-in-range",
+	FACTOR_QUANTILE_NOT_IN_RANGE = "factor-quantile-not-in-range",
+	FACTOR_STDEV_NOT_IN_RANGE = "factor-stdev-not-in-range",
+	FACTOR_COMMON_VALUE_NOT_IN_RANGE = "factor-common-value-not-in-range",
 
 	// for string type
-	FACTOR_IS_BLANK = 'factor-is-blank',
-	FACTOR_STRING_LENGTH_MISMATCH = 'factor-string-length-mismatch',
-	FACTOR_STRING_LENGTH_NOT_IN_RANGE = 'factor-string-length-not-in-range',
-	FACTOR_MATCH_REGEXP = 'factor-match-regexp',
-	FACTOR_MISMATCH_REGEXP = 'factor-mismatch-regexp',
+	FACTOR_IS_BLANK = "factor-is-blank",
+	FACTOR_STRING_LENGTH_MISMATCH = "factor-string-length-mismatch",
+	FACTOR_STRING_LENGTH_NOT_IN_RANGE = "factor-string-length-not-in-range",
+	FACTOR_MATCH_REGEXP = "factor-match-regexp",
+	FACTOR_MISMATCH_REGEXP = "factor-mismatch-regexp",
 
 	// for 2 factors
-	FACTOR_AND_ANOTHER = 'factor-and-another'
+	FACTOR_AND_ANOTHER = "factor-and-another",
 }
 
 export enum MonitorRuleSeverity {
-	FATAL = 'fatal',
-	WARN = 'warn',
-	TRACE = 'trace'
+	FATAL = "fatal",
+	WARN = "warn",
+	TRACE = "trace",
 }
 
 export enum MonitorRuleStatisticalInterval {
-	DAILY = 'daily',
-	WEEKLY = 'weekly',
-	MONTHLY = 'monthly'
+	DAILY = "daily",
+	WEEKLY = "weekly",
+	MONTHLY = "monthly",
 }
 
 export enum MonitorRuleCompareOperator {
-	EQUAL = 'eq',
-	LESS_THAN = 'lt',
-	LESS_THAN_OR_EQUAL = 'lte',
-	GREATER_THAN = 'gt',
-	GREATER_THAN_EQUAL = 'gte'
+	EQUAL = "eq",
+	LESS_THAN = "lt",
+	LESS_THAN_OR_EQUAL = "lte",
+	GREATER_THAN = "gt",
+	GREATER_THAN_EQUAL = "gte",
 }
 
 export interface MonitorRuleParameters {
@@ -87,7 +87,7 @@ export interface MonitorRuleParameters {
 }
 
 export interface MonitorRule {
-	uid?: string;
+	ruleId?: string;
 	code: MonitorRuleCode;
 	grade: MonitorRuleGrade;
 	severity: MonitorRuleSeverity;
@@ -110,7 +110,7 @@ export const GlobalRuleDefs = [
 	MonitorRuleCode.FACTOR_MISMATCH_ENUM,
 	MonitorRuleCode.FACTOR_MISMATCH_DATE_TYPE,
 
-	MonitorRuleCode.ROWS_NOT_EXISTS
+	MonitorRuleCode.ROWS_NOT_EXISTS,
 ];
 export const TopicRuleDefs = [
 	MonitorRuleCode.RAW_MISMATCH_STRUCTURE,
@@ -120,7 +120,7 @@ export const TopicRuleDefs = [
 
 	MonitorRuleCode.ROWS_NOT_EXISTS,
 	MonitorRuleCode.ROWS_NO_CHANGE,
-	MonitorRuleCode.ROWS_COUNT_MISMATCH_AND_ANOTHER
+	MonitorRuleCode.ROWS_COUNT_MISMATCH_AND_ANOTHER,
 ];
 export const FactorRuleDefs = [
 	MonitorRuleCode.FACTOR_MISMATCH_TYPE,
@@ -149,7 +149,7 @@ export const FactorRuleDefs = [
 	MonitorRuleCode.FACTOR_MATCH_REGEXP,
 	MonitorRuleCode.FACTOR_MISMATCH_REGEXP,
 
-	MonitorRuleCode.FACTOR_AND_ANOTHER
+	MonitorRuleCode.FACTOR_AND_ANOTHER,
 ];
 
 export interface MonitorRuleLogCriteria {
