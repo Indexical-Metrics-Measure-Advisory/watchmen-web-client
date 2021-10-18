71b4e7ab11732d78f5243bdf0297e0dcefbdd1e1
botlikes <bradwoo8621@yandex.com>
2021-10-18 23:15:42 +0800

    funnels

--- src/widgets/funnel/year/index.tsx
+++ src/widgets/funnel/year/index.tsx
@@ -1,13 +1,25 @@
-import {ReportFunnel} from '@/services/data/tuples/report-types';
+import {ReportFunnel, ReportFunnelType} from '@/services/data/tuples/report-types';
+import dayjs from 'dayjs';
 import React, {ReactNode} from 'react';
-import {PairYearEditor} from './pair';
-import {SingleYearEditor} from './single';
+import {DropdownOption} from '../../basic/types';
+import {DropdownEditor} from '../dropdown';
+
+const MaxFutureYears = 5;
+const MaxHistoryYears = 30;
+const ThisYear = dayjs().year();
+const YearOptions: Array<DropdownOption> = [
+	...new Array(MaxFutureYears).fill(1).map((_, index) => ThisYear + MaxFutureYears - index),
+	ThisYear,
+	...new Array(MaxHistoryYears).fill(1).map((_, index) => ThisYear - MaxHistoryYears + index).reverse()
+].map(year => {
+	return {
+		value: year, label: `${year}`
+	};
+});
 
 export const YearEditor = (props: { funnel: ReportFunnel; pairJoint: ReactNode }) => {
 	const {funnel, pairJoint} = props;
 
-	return <>
-		<SingleYearEditor funnel={funnel}/>
-		<PairYearEditor funnel={funnel} pairJoint={pairJoint}/>
-	</>;
+	return <DropdownEditor funnel={funnel} acceptedType={ReportFunnelType.YEAR} options={YearOptions}
+	                       pairJoint={pairJoint}/>;
 };
