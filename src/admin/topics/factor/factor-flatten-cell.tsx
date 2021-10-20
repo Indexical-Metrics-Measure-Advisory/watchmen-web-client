33de28a5d2e45269a5228e1a1d808979a1c8fa6b
botlikes <bradwoo8621@yandex.com>
2021-10-20 11:04:07 +0800

    import/export factors

--- src/admin/topics/factor/factor-flatten-cell.tsx
+++ src/admin/topics/factor/factor-flatten-cell.tsx
@@ -1,4 +1,4 @@
-import {Factor} from '@/services/data/tuples/factor-types';
+import {Factor, FactorType} from '@/services/data/tuples/factor-types';
 import {isNotRawTopic} from '@/services/data/tuples/topic';
 import {Topic} from '@/services/data/tuples/topic-types';
 import {useForceUpdate} from '@/widgets/basic/utils';
@@ -7,6 +7,40 @@ import {useTopicEventBus} from '../topic-event-bus';
 import {TopicEventTypes} from '../topic-event-bus-types';
 import {FactorFlattenCellContainer, FactorPropCheckBox, FactorPropLabel} from './widgets';
 
+const isSubFactorCanBeFlatten = (topic: Topic, parentFactorName: string): boolean => {
+	const parentFactor = (topic.factors || []).find(factor => factor.name === parentFactorName);
+	if (!parentFactor) {
+		return true;
+	} else if (parentFactor.type === FactorType.ARRAY) {
+		return false;
+	} else if (!parentFactorName.includes('.')) {
+		return true;
+	} else {
+		const [, ...names] = parentFactorName.split('.').reverse();
+		return isSubFactorCanBeFlatten(topic, names.reverse().join('.'));
+	}
+};
+const isFactorCanBeFlatten = (topic: Topic, factor: Factor): boolean => {
+	if (isNotRawTopic(topic)) {
+		return false;
+	}
+
+	if (!factor.name.includes('.')) {
+		return false;
+	}
+	if (factor.type === FactorType.OBJECT || factor.type === FactorType.ARRAY) {
+		return false;
+	}
+
+	const name = factor.name || '';
+	if (!name) {
+		return true;
+	}
+
+	const [, ...names] = name.split('.').reverse();
+	return isSubFactorCanBeFlatten(topic, names.reverse().join('.'));
+};
+
 export const FactorFlattenCell = (props: { topic: Topic, factor: Factor }) => {
 	const {topic, factor} = props;
 
@@ -14,7 +48,7 @@ export const FactorFlattenCell = (props: { topic: Topic, factor: Factor }) => {
 	const forceUpdate = useForceUpdate();
 	useEffect(() => {
 		const onTopicTypeChanged = () => {
-			if (isNotRawTopic(topic)) {
+			if (!isFactorCanBeFlatten(topic, factor)) {
 				delete factor.flatten;
 			}
 			forceUpdate();
@@ -27,7 +61,7 @@ export const FactorFlattenCell = (props: { topic: Topic, factor: Factor }) => {
 	useEffect(() => {
 		const onFactorNameChanged = (changed: Factor) => {
 			if (changed === factor) {
-				if (changed.name.includes('.')) {
+				if (!isFactorCanBeFlatten(topic, factor)) {
 					delete changed.flatten;
 				}
 				forceUpdate();
@@ -37,21 +71,21 @@ export const FactorFlattenCell = (props: { topic: Topic, factor: Factor }) => {
 		return () => {
 			off(TopicEventTypes.FACTOR_NAME_CHANGED, onFactorNameChanged);
 		};
+	}, [on, off, topic, factor, forceUpdate]);
+	useEffect(() => {
+		const onFactorTypeChanged = (changed: Factor) => {
+			if (changed === factor || factor.name.startsWith(`${changed.name}.`)) {
+				if (!isFactorCanBeFlatten(topic, factor)) {
+					delete changed.flatten;
+				}
+				forceUpdate();
+			}
+		};
+		on(TopicEventTypes.FACTOR_TYPE_CHANGED, onFactorTypeChanged);
+		return () => {
+			off(TopicEventTypes.FACTOR_TYPE_CHANGED, onFactorTypeChanged);
+		};
 	}, [on, off, factor, forceUpdate]);
-	// useEffect(() => {
-	// 	const onFactorTypeChanged = (changed: Factor) => {
-	// 		if (changed === factor) {
-	// 			if ([FactorType.OBJECT, FactorType.ARRAY].includes(changed.type)) {
-	// 				delete changed.flatten;
-	// 			}
-	// 			forceUpdate();
-	// 		}
-	// 	};
-	// 	on(TopicEventTypes.FACTOR_TYPE_CHANGED, onFactorTypeChanged);
-	// 	return () => {
-	// 		off(TopicEventTypes.FACTOR_TYPE_CHANGED, onFactorTypeChanged);
-	// 	};
-	// }, [on, off, factor, forceUpdate]);
 
 	const onFactorFlattenChange = (value: boolean) => {
 		if (value) {
@@ -63,7 +97,7 @@ export const FactorFlattenCell = (props: { topic: Topic, factor: Factor }) => {
 		fire(TopicEventTypes.FACTOR_FLATTEN_CHANGED, factor);
 	};
 
-	if (isNotRawTopic(topic) || factor.name.includes('.')) {
+	if (!isFactorCanBeFlatten(topic, factor)) {
 		return null;
 	}
 
