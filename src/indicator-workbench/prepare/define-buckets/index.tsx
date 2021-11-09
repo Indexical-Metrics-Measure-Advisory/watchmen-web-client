95ffae1cab657df0585333d2ac594724a8b380b4
botlikes <bradwoo8621@yandex.com>
2021-11-09 17:54:09 +0800

    indicator workbench

--- src/indicator-workbench/prepare/define-buckets/index.tsx
+++ src/indicator-workbench/prepare/define-buckets/index.tsx
@@ -1,4 +1,6 @@
 import {ButtonInk} from '@/widgets/basic/types';
+import {useEventBus} from '@/widgets/events/event-bus';
+import {EventTypes} from '@/widgets/events/types';
 import {Lang} from '@/widgets/langs';
 import {useIndicatorsEventBus} from '../indicators-event-bus';
 import {IndicatorsEventTypes} from '../indicators-event-bus-types';
@@ -16,6 +18,7 @@ import {PrepareStep} from '../types';
 import {useConstructed} from '../use-constructed';
 
 export const DefineBuckets = () => {
+	const {fire: fireGlobal} = useEventBus();
 	const {fire} = useIndicatorsEventBus();
 	const {constructed, setConstructed, visible, setVisible} = useConstructed();
 	const {data, done} = useStep({
@@ -30,6 +33,8 @@ export const DefineBuckets = () => {
 	}
 
 	const onDefineClicked = () => {
+		// TODO define buckets
+		fireGlobal(EventTypes.SHOW_NOT_IMPLEMENT);
 	};
 	const onIgnoreDefineClicked = () => {
 		fire(IndicatorsEventTypes.SWITCH_STEP, PrepareStep.SAVE_INDICATOR, data);
