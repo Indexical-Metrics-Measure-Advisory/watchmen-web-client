f029d77294c88d9fc615afc71bb2fb9a1fd9ff25
botlikes <bradwoo8621@yandex.com>
2022-01-19 11:52:24 +0800

    delay 1 second to wait echarts done

--- src/widgets/report/diagram/echart-diagram.tsx
+++ src/widgets/report/diagram/echart-diagram.tsx
@@ -26,28 +26,30 @@ export const EChartDiagram = (props: { report: Report; thumbnail: boolean }) => 
 			const onFinished = () => {
 				fire(ReportEventTypes.REPAINTED, report);
 				instance.off('finished', onFinished);
-				if (thumbnail) {
-					const image = new Image();
-					image.onload = () => {
-						const canvas = document.createElement('canvas');
-						canvas.width = 400;
-						canvas.height = 300;
-						const ctx = canvas.getContext('2d');
-						let width = image.width;
-						let height = image.height;
-						if (width > canvas.width || height > canvas.height) {
-							const ratio = Math.min(canvas.width / width, canvas.height / height);
-							width = width * ratio;
-							height = height * ratio;
-						}
-						const dx = (canvas.width - width) / 2;
-						const dy = (canvas.height - height) / 2;
-						ctx?.drawImage(image, dx, dy, width, height);
-						report.simulateThumbnail = canvas.toDataURL('png', {pixelRatio: window.devicePixelRatio});
-						fire(ReportEventTypes.THUMBNAIL_CAUGHT, report);
-					};
-					image.src = instance.getDataURL({type: 'png', pixelRatio: window.devicePixelRatio});
-				}
+				setTimeout(() => {
+					if (thumbnail) {
+						const image = new Image();
+						image.onload = () => {
+							const canvas = document.createElement('canvas');
+							canvas.width = 400;
+							canvas.height = 300;
+							const ctx = canvas.getContext('2d');
+							let width = image.width;
+							let height = image.height;
+							if (width > canvas.width || height > canvas.height) {
+								const ratio = Math.min(canvas.width / width, canvas.height / height);
+								width = width * ratio;
+								height = height * ratio;
+							}
+							const dx = (canvas.width - width) / 2;
+							const dy = (canvas.height - height) / 2;
+							ctx?.drawImage(image, dx, dy, width, height);
+							report.simulateThumbnail = canvas.toDataURL('png', {pixelRatio: window.devicePixelRatio});
+							fire(ReportEventTypes.THUMBNAIL_CAUGHT, report);
+						};
+						image.src = instance.getDataURL({type: 'png', pixelRatio: window.devicePixelRatio});
+					}
+				}, 1000)
 			};
 			instance.on('finished', onFinished);
 			instance.clear();
