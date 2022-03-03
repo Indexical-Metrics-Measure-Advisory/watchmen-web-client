9f7ba1e4995925848c8739901b590e267de1ba70
botlikes <bradwoo8621@yandex.com>
2022-03-03 19:43:25 +0800

    fixed: cannot detect the ending of drop indexes of given topic

--- src/admin/topics/script-generation/mysql-sql-alteration.ts
+++ src/admin/topics/script-generation/mysql-sql-alteration.ts
@@ -94,13 +94,14 @@ ${buildAuditTimeColumn(topic, getUpdateTimeColumnName())}
 	-- simply uncomment the following loop to drop all exists indexes
 	-- considering performance of rebuild indexes, manually drop useless indexes accurate is recommended.
 	-- according to duplication check of index names, following create scripts need to be adjusted manually as well.
-	-- SELECT INDEX_NAME INTO @indexName FROM INFORMATION_SCHEMA.STATISTICS WHERE TABLE_SCHEMA = CurrentDatabase AND TABLE_NAME = '${tableName}' AND INDEX_NAME <> 'PRIMARY' LIMIT 1;
-	-- WHILE @indexName IS NOT NULL DO
+	-- SELECT COUNT(1) INTO @indexCount FROM INFORMATION_SCHEMA.STATISTICS WHERE TABLE_SCHEMA = CurrentDatabase AND TABLE_NAME = '${tableName}' AND INDEX_NAME <> 'PRIMARY' LIMIT 1;
+	-- WHILE @indexCount != 0 DO
+	--	SELECT INDEX_NAME INTO @indexName FROM INFORMATION_SCHEMA.STATISTICS WHERE TABLE_SCHEMA = CurrentDatabase AND TABLE_NAME = '${tableName}' AND INDEX_NAME <> 'PRIMARY' LIMIT 1;
 	--	SET @sql = concat('DROP INDEX ', @indexName, ' ON ${tableName};');
 	--	PREPARE stmt FROM @sql;
 	--	EXECUTE stmt;
 	--	DEALLOCATE PREPARE stmt;
-	--	SELECT INDEX_NAME INTO @indexName FROM INFORMATION_SCHEMA.STATISTICS WHERE TABLE_SCHEMA = CurrentDatabase AND TABLE_NAME = '${tableName}' AND INDEX_NAME <> 'PRIMARY' LIMIT 1;
+	--	SELECT COUNT(1) INTO @indexCount FROM INFORMATION_SCHEMA.STATISTICS WHERE TABLE_SCHEMA = CurrentDatabase AND TABLE_NAME = '${tableName}' AND INDEX_NAME <> 'PRIMARY' LIMIT 1;
 	-- END WHILE;
 	
 	-- unique index
